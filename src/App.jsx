337e71e5197e8d08da330d829b1deadb327faf48
Matttash <matthewgitau1@gmail.com>
2026-05-18 10:30:45 -0400

    Final project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
-import Notification from "./Components/Notification/Notification";
-
 function App() {
-  return (
-    <Router>
-      <Notification>
-        <Routes>
-          <Route
-            path="/instant-consultation"
-            element={<InstantConsultation />}
-          />
-        </Routes>
-      </Notification>
-    </Router>
-  );
-}
-
-export default App;
+    return <h1>APP IS WORKING</h1>;
+  }
+  
+  export default App;
