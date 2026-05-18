5dd4af5f23d567653458414af29d74b697f11e11
Matttash <matthewgitau1@gmail.com>
2026-05-18 14:29:57 -0400

    Restored App.jsx with Notification component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
+import Notification from "./Components/Notification/Notification";
+import Navbar from "./Components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>StayHealthy Medical App</h1>
+      <p>Book appointments with doctors online</p>
+    </div>
+  );
+}
+
 function App() {
-    return <h1>APP IS WORKING</h1>;
-  }
-  
-  export default App;
+  return (
+    <Router>
+      <Notification>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
+        </Routes>
+      </Notification>
+    </Router>
+  );
+}
+
+export default App;
